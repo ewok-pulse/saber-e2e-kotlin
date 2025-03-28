092002a62dc258be75aa2e6ce4f63c7a62b200db
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2025-03-29 01:59:53 +0900

    [Analysis API] Add 'kotlin.Cloneable' for common sources
    In the compiler, the 'FirCloneableSymbolProvider' is added both to JVM
    and Common source sets (see 'prepareMetadataSessions()'). This commit
    brings the same behavior to the Analysis API.
    
    ^KT-74448 Fixed

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/factory/LLFirBuiltinsSessionFactory.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/factory/LLFirBuiltinsSessionFactory.kt
@@ -35,8 +35,7 @@ import org.jetbrains.kotlin.fir.session.registerJavaComponents
 import org.jetbrains.kotlin.fir.session.registerModuleData
 import org.jetbrains.kotlin.fir.symbols.FirLazyDeclarationResolver
 import org.jetbrains.kotlin.platform.TargetPlatform
-import org.jetbrains.kotlin.platform.has
-import org.jetbrains.kotlin.platform.jvm.JvmPlatform
+import org.jetbrains.kotlin.platform.isCommon
 import org.jetbrains.kotlin.platform.jvm.isJvm
 import org.jetbrains.kotlin.resolve.jvm.modules.JavaModuleResolver
 import java.util.concurrent.ConcurrentHashMap
@@ -120,7 +119,7 @@ class LLFirBuiltinsSessionFactory(private val project: Project) {
                 )
 
                 add(FirExtensionSyntheticFunctionInterfaceProvider(session, moduleData, kotlinScopeProvider))
-                if (platform.has<JvmPlatform>()) {
+                if (platform.isCommon() || platform.isJvm()) {
                     add(FirCloneableSymbolProvider(session, moduleData, kotlinScopeProvider))
                 }
             }

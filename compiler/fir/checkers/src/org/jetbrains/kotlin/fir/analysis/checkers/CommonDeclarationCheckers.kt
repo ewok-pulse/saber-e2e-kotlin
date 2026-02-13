753e646ab9e8580aaa947aeb70d9db901782dc26
Matej Tomášek <matej.tomasek@jetbrains.com>
2026-02-13 11:57:13 +0100

    Rebasing the old branch

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonDeclarationCheckers.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonDeclarationCheckers.kt
@@ -183,6 +183,7 @@ object CommonDeclarationCheckers : DeclarationCheckers() {
         FirTopLevelPropertiesChecker,
         FirPackageConflictsWithClassifierChecker,
         PlatformClassMappedToKotlinImportsChecker,
+        FirStaticInitializationChecker
     )
 
     override val scriptCheckers: Set<FirScriptChecker> = setOf(

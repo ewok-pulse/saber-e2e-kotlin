457771b4b6ce8563c6502817d5131254b10a6b30
YuliaKorolenko <korollenkoyullia@gmail.com>
2026-03-04 15:16:04 +0100

    Fix: Must be compared the closest no-inline parent nodes in cfg

--- analysis/analysis-api-fir/gen/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KaFirDataClassConverters.kt
+++ analysis/analysis-api-fir/gen/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KaFirDataClassConverters.kt
@@ -4524,6 +4524,13 @@ private fun KaDiagnosticConverterBuilder.addConversions101() {
 }
 
 private fun KaDiagnosticConverterBuilder.addConversions102() {
+    add(FirErrors.CV_DIAGNOSTIC) { firDiagnostic ->
+        CvDiagnosticImpl(
+            firDiagnostic.a,
+            firDiagnostic as KtPsiDiagnostic,
+            token,
+        )
+    }
     add(FirErrors.UNRESOLVED_REFERENCE) { firDiagnostic ->
         UnresolvedReferenceImpl(
             firDiagnostic.a,

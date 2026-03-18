015a86fc5273678df6166bfabf8ab3c71a7e5ff9
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2026-03-18 13:37:22 +0100

    [Test] Reproduce "False positive REDUNDANT_CALL_OF_CONVERSION_METHOD in case of overloads"
    KT-80060

---
+++ compiler/fir/analysis-tests/testData/resolve/extraCheckers/RedundantCallOfConversionMethod/noDiagIfMemberAndExtFuncOverloads.kt
@@ -0,0 +1,20 @@
+// RUN_PIPELINE_TILL: BACKEND
+// ISSUE: KT-80060
+
+class C {
+    fun test(x: Long) = "call from member"
+}
+
+fun C.test(x: Int) = "call from extension"
+
+fun test() {
+    val c = C()
+    c.test(1.<!REDUNDANT_CALL_OF_CONVERSION_METHOD!>toInt()<!>) // The `toInt` call forces resolving to the extension, the REDUNDANT_CALL_OF_CONVERSION_METHOD shouldn't be reported here
+    c.test((2 + 3).<!REDUNDANT_CALL_OF_CONVERSION_METHOD!>toInt()<!>) // Resolving to the extension, no REDUNDANT_CALL_OF_CONVERSION_METHOD
+    c.test('d'.<!DEPRECATION!>toInt<!>()) // Dispatch receiver is not `Int` -> always non-redundant, resolving to the member
+    c.test(4) // The integer literal type is treated as Long here and it forces the call to be resolved to the member
+    c.test(5L) // Long literal, no ambiguity, resolving to the member
+}
+
+/* GENERATED_FIR_TAGS: classDeclaration, funWithExtensionReceiver, functionDeclaration, integerLiteral, localProperty,
+propertyDeclaration, stringLiteral */

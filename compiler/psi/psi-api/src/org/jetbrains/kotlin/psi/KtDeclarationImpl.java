b93bb0de93e94dc80b70eedbec8d090fe08bfd7b
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-04-17 00:05:27 +0400

    [PSI] Move modifier mutations to the service
    Move modifier-list, modifier, and annotation mutations behind KtPsiMutatingService, including the primary-constructor-specific variants.
    
    KT-85052

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtDeclarationImpl.java
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtDeclarationImpl.java
@@ -22,7 +22,6 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.KtNodeTypes;
 import org.jetbrains.kotlin.kdoc.psi.api.KDoc;
 import org.jetbrains.kotlin.lexer.KtModifierKeywordToken;
-import org.jetbrains.kotlin.psi.addRemoveModifier.AddRemoveModifierKt;
 import org.jetbrains.kotlin.psi.findDocComment.FindDocCommentKt;
 
 import java.util.Collections;
@@ -45,20 +44,32 @@ public abstract class KtDeclarationImpl extends KtExpressionImpl implements KtDe
         return modifierList != null && modifierList.hasModifier(modifier);
     }
 
+    /**
+     * @deprecated Use {@code KtPsiMutatingService.getInstance().addModifier(this, modifier)} instead.
+     */
     @Override
+    @Deprecated
     public void addModifier(@NotNull KtModifierKeywordToken modifier) {
-        AddRemoveModifierKt.addModifier(this, modifier);
+        KtPsiMutatingService.getInstance().addModifier(this, modifier);
     }
 
+    /**
+     * @deprecated Use {@code KtPsiMutatingService.getInstance().removeModifier(this, modifier)} instead.
+     */
     @Override
+    @Deprecated
     public void removeModifier(@NotNull KtModifierKeywordToken modifier) {
-        AddRemoveModifierKt.removeModifier(this, modifier);
+        KtPsiMutatingService.getInstance().removeModifier(this, modifier);
     }
 
+    /**
+     * @deprecated Use {@code KtPsiMutatingService.getInstance().addAnnotationEntry(this, annotationEntry)} instead.
+     */
     @NotNull
     @Override
+    @Deprecated
     public KtAnnotationEntry addAnnotationEntry(@NotNull KtAnnotationEntry annotationEntry) {
-        return AddRemoveModifierKt.addAnnotationEntry(this, annotationEntry);
+        return KtPsiMutatingService.getInstance().addAnnotationEntry(this, annotationEntry);
     }
 
     @NotNull

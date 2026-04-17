1c3a1c61c6ac5f40988d62e877880c02f12d6bb1
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-04-17 08:59:04 +0200

    fixup! Drop legacy (K1) ScriptingCompilerConfigurationComponentRegistrar

--- plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/pluginRegisrar.kt
+++ plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/pluginRegisrar.kt
@@ -7,11 +7,15 @@
 
 package org.jetbrains.kotlin.scripting.compiler.plugin
 
+import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
+import org.jetbrains.kotlin.cli.extensionsStorage
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.compiler.plugin.registerExtension
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
 import org.jetbrains.kotlin.fir.extensions.CollectAdditionalSourceFilesExtension
+import org.jetbrains.kotlin.scripting.compiler.plugin.extensions.ReplLoweringExtension
+import org.jetbrains.kotlin.scripting.compiler.plugin.extensions.ScriptLoweringExtension
 import org.jetbrains.kotlin.scripting.compiler.plugin.fir.CollectAdditionalScriptSourcesExtension
 
 // Scripting infrastructure still depends on project-based components, therefore we still need a separate registrar above - ScriptingCompilerConfigurationComponentRegistrar
@@ -21,6 +25,11 @@ class ScriptingK2CompilerPluginRegistrar : CompilerPluginRegistrar() {
         fun registerComponents(extensionStorage: ExtensionStorage, compilerConfiguration: CompilerConfiguration) = with(extensionStorage) {
             FirExtensionRegistrar.registerExtension(FirScriptingCompilerExtensionRegistrar(compilerConfiguration))
             FirExtensionRegistrar.registerExtension(FirScriptingSamWithReceiverExtensionRegistrar())
+
+            with(compilerConfiguration.extensionsStorage!!) {
+                IrGenerationExtension.registerExtension(ScriptLoweringExtension())
+                IrGenerationExtension.registerExtension(ReplLoweringExtension())
+            }
         }
     }
 

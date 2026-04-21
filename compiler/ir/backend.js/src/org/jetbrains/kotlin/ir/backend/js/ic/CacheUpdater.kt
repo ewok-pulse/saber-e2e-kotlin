5e9c589f1e5a9ac2fbdf36c4d45d40e525b53a40
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-04-21 11:39:08 +0200

    [JS] Rewrite `createBackendContext` to avoid usage of `IrBuiltInsOverDescriptors`
    We will start using `IrBuiltInsOverLinker` in later commits.
    
    #KT-84836

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/ic/CacheUpdater.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/ic/CacheUpdater.kt
@@ -16,6 +16,7 @@ import org.jetbrains.kotlin.ir.declarations.IrFactory
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.util.IdSignature
+import org.jetbrains.kotlin.ir.util.SymbolTable
 import org.jetbrains.kotlin.js.config.JsGenerationGranularity
 import org.jetbrains.kotlin.js.config.WebArtifactConfiguration
 import org.jetbrains.kotlin.js.config.includes
@@ -67,6 +68,7 @@ interface PlatformDependentICContext {
     fun createBackendContext(
         mainModule: IrModuleFragment,
         irBuiltIns: IrBuiltIns,
+        symbolTable: SymbolTable,
         configuration: CompilerConfiguration,
     ): JsCommonBackendContext
 
@@ -767,7 +769,7 @@ class CacheUpdater(
 
         stopwatch.startNext("Processing IR - initializing the compiler")
         val mainModuleFragment = loadedIr.orderedFragments[mainLibraryFile] ?: notFoundIcError("main module fragment", mainLibraryFile)
-        val compilerContext = icContext.createBackendContext(mainModuleFragment, loadedIr.irBuiltIns, compilerConfiguration)
+        val compilerContext = icContext.createBackendContext(mainModuleFragment, loadedIr.irBuiltIns, loadedIr.symbolTable, compilerConfiguration)
         val compilerForIC = icContext.createCompiler(mainModuleFragment, loadedIr.irBuiltIns, compilerConfiguration, compilerContext)
 
         val dirtyFiles = dirtyFileExports.entries.associateTo(newHashMapWithExpectedSize(dirtyFileExports.size)) {
@@ -864,7 +866,7 @@ fun rebuildCacheForDirtyFiles(
         currentIrModule.files.memoryOptimizedFilter { irFile -> irFile.fileEntry.name in files }
     } ?: currentIrModule.files
 
-    val backendContext = icContext.createBackendContext(currentIrModule, loadedIr.irBuiltIns, configuration)
+    val backendContext = icContext.createBackendContext(currentIrModule, loadedIr.irBuiltIns, loadedIr.symbolTable, configuration)
     val compilerWithIC = icContext.createCompiler(currentIrModule, loadedIr.irBuiltIns, configuration, backendContext)
 
     irInterner.reset()

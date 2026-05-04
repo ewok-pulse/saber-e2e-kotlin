joshfriend/kotlin

[BTA] Introduce Wasm toolchain
Features include:
* klib compilation phase,
* linking phase,
* incremental compilation

^KT-78206 Verification pending
^KT-78207 Verification pending

compiler/build-tools/kotlin-build-tools-api/src/main/kotlin/org/jetbrains/kotlin/buildtools/api/wasm/operations/WasmKlibCompilationOperation.kt
@@ -1,0 +1,148 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.buildtools.api.wasm.operations
+
+import org.jetbrains.kotlin.buildtools.api.BaseCompilationOperation
+import org.jetbrains.kotlin.buildtools.api.CancellableBuildOperation
+import org.jetbrains.kotlin.buildtools.api.CompilationResult
+import org.jetbrains.kotlin.buildtools.api.ExperimentalBuildToolsApi
+import org.jetbrains.kotlin.buildtools.api.SourcesChanges
+import org.jetbrains.kotlin.buildtools.api.arguments.ExperimentalCompilerArgument
+import org.jetbrains.kotlin.buildtools.api.arguments.WasmArguments
+import org.jetbrains.kotlin.buildtools.api.internal.BaseOption
+import org.jetbrains.kotlin.buildtools.api.js.IncrementalModule
+import org.jetbrains.kotlin.buildtools.api.wasm.WasmHistoryBasedIncrementalCompilationConfiguration
+import org.jetbrains.kotlin.buildtools.api.wasm.WasmIncrementalCompilationConfiguration
+import java.nio.file.Path
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+/**
+ * Compiles Kotlin code targeting JS platform and using specified options into a klib.
+ *
+ * This interface is not intended to be implemented by the API consumers.
+ *
+ * Obtain an instance of this interface from [WasmPlatformToolchain.wasmKlibCompilationOperationBuilder].
+ *
+ * @since 2.4.20
+ */
+@ExperimentalBuildToolsApi
+public interface WasmKlibCompilationOperation : BaseCompilationOperation, CancellableBuildOperation<CompilationResult> {
+
+    /**
+     * All sources of the compilation unit.
+     */
+    public val sources: List<Path>
+
+    /**
+     * The directory where the resulting klib will be placed.
+     */
+    public val destination: Path
+
+    @OptIn(ExperimentalCompilerArgument::class)
+    public val compilerArguments: WasmArguments
+
+    /**
+     * A builder for configuring and instantiating the [WasmKlibCompilationOperation].
+     */
+    public interface Builder : BaseCompilationOperation.Builder {
+        /**
+         * All sources of the compilation unit.
+         */
+        public val sources: List<Path>
+
+        /**
+         * Output klib file.
+         */
+        public val destination: Path
+
+        /**
+         * Kotlin compiler configurable options for klib-based compilation.
+         */
+        @OptIn(ExperimentalCompilerArgument::class)
+        public val compilerArguments: WasmArguments.Builder
+
+        /**
+         * Creates the configuration object for history-based incremental compilation (IC) in JS projects.
+         *
+         * @param rootProjectDir the root directory of the project
+         * @param workingDirectory the directory where the compiler will store the incremental compilation caches
+         * @param sourcesChanges the changes in the source files, which can be unknown, to-be-calculated, or known
+         * @param modulesInformation the information about modules layout in the project
+         */
+        public fun historyBasedIcConfigurationBuilder(
+            rootProjectDir: Path,
+            workingDirectory: Path,
+            sourcesChanges: SourcesChanges,
+            modulesInformation: List<IncrementalModule>,
+        ): WasmHistoryBasedIncrementalCompilationConfiguration.Builder
+
+        /**
+         * Get the value for option specified by [key] if it was previously [set] or if it has a default value.
+         *
+         * @return the previously set value for an option
+         * @throws IllegalStateException if the option was not set and has no default value
+         */
+        public operator fun <V> get(key: Option<V>): V
+
+        /**
+         * Set the [value] for option specified by [key], overriding any previous value for that option.
+         */
+        public operator fun <V> set(key: Option<V>, value: V)
+
+        /**
+         * Creates an immutable instance of [WasmKlibCompilationOperation] based on the configuration of this builder.
+         */
+        public fun build(): WasmKlibCompilationOperation
+    }
+
+    /**
+     * Returns a [Builder] initialized with the values of this [WasmKlibCompilationOperation].
+     */
+    public fun toBuilder(): Builder
+
+    /**
+     * An option for configuring a [WasmKlibCompilationOperation].
+     */
+    public class Option<V> internal constructor(id: String) : BaseOption<V>(id)
+
+    /**
+     * Get the value for option specified by [key] if it was previously [set] or if it has a default value.
+     *
+     * @return the previously set value for an option
+     * @throws IllegalStateException if the option was not set and has no default value
+     */
+    public operator fun <V> get(key: Option<V>): V
+
+    public companion object {
+        @JvmField
+        public val INCREMENTAL_COMPILATION: Option<WasmIncrementalCompilationConfiguration?> = Option("INCREMENTAL_COMPILATION")
+    }
+}
+
+
+/**
+ * Convenience function for creating a [WasmKlibCompilationOperation] with options configured by [builderAction].
+ *
+ * @return an immutable `WasmKlibCompilationOperation`.
+ * @see WasmPlatformToolchain.wasmKlibCompilationOperationBuilder
+ */
+@OptIn(ExperimentalContracts::class)
+@ExperimentalBuildToolsApi
+public inline fun WasmKlibCompilationOperation.Builder.historyBasedIcConfiguration(
+    rootProjectDir: Path,
+    workingDirectory: Path,
+    sourcesChanges: SourcesChanges,
+    modulesInformation: List<IncrementalModule>,
+    builderAction: WasmHistoryBasedIncrementalCompilationConfiguration.Builder.() -> Unit = {},
+): WasmHistoryBasedIncrementalCompilationConfiguration {
+    contract {
+        callsInPlace(builderAction, InvocationKind.EXACTLY_ONCE)
+    }
+    return historyBasedIcConfigurationBuilder(rootProjectDir, workingDirectory, sourcesChanges, modulesInformation).apply(builderAction)
+        .build()
+}

compiler/build-tools/kotlin-build-tools-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/wasm/WasmHistoryBasedIncrementalCompilationConfigurationImpl.kt
@@ -1,0 +1,90 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("DEPRECATION")
+
+package org.jetbrains.kotlin.buildtools.internal.wasm
+
+import org.jetbrains.kotlin.buildtools.api.SourcesChanges
+import org.jetbrains.kotlin.buildtools.api.js.IncrementalModule
+import org.jetbrains.kotlin.buildtools.api.wasm.WasmHistoryBasedIncrementalCompilationConfiguration
+import org.jetbrains.kotlin.buildtools.internal.*
+import org.jetbrains.kotlin.incremental.IncrementalCompilerRunner
+import java.nio.file.Path
+
+internal class WasmHistoryBasedIncrementalCompilationConfigurationImpl private constructor(
+    override val workingDirectory: Path,
+    override val sourcesChanges: SourcesChanges,
+    override val modulesInformation: List<IncrementalModule>,
+    override val options: Options = Options(
+        WasmHistoryBasedIncrementalCompilationConfiguration::class
+    ),
+) : BaseIncrementalCompilationConfigurationImpl(), WasmHistoryBasedIncrementalCompilationConfiguration,
+    WasmHistoryBasedIncrementalCompilationConfiguration.Builder,
+    DeepCopyable<WasmHistoryBasedIncrementalCompilationConfigurationImpl> {
+
+    constructor(
+        rootProjectDir: Path,
+        workingDirectory: Path,
+        sourcesChanges: SourcesChanges,
+        modulesInformation: List<IncrementalModule>,
+    ) : this(
+        workingDirectory,
+        sourcesChanges,
+        modulesInformation,
+        Options(WasmHistoryBasedIncrementalCompilationConfiguration::class)
+    ) {
+        initializeOptions(this::class, options)
+        options[ROOT_PROJECT_DIR] = rootProjectDir
+    }
+
+    internal val historyFile: Path
+        get() {
+            return (this[HISTORY_FILE_DIR] ?: workingDirectory).resolve(
+                IncrementalCompilerRunner.BUILD_HISTORY_FILE_NAME
+            )
+        }
+
+    override fun build(): WasmHistoryBasedIncrementalCompilationConfiguration = deepCopy()
+
+    override fun toBuilder(): WasmHistoryBasedIncrementalCompilationConfiguration.Builder = deepCopy()
+
+    override fun deepCopy(): WasmHistoryBasedIncrementalCompilationConfigurationImpl =
+        WasmHistoryBasedIncrementalCompilationConfigurationImpl(
+            workingDirectory,
+            sourcesChanges,
+            modulesInformation,
+            options.deepCopy()
+        )
+
+
+    @UseFromImplModuleRestricted
+    override fun <V> get(key: WasmHistoryBasedIncrementalCompilationConfiguration.Option<V>): V {
+        return options[key]
+    }
+
+    @UseFromImplModuleRestricted
+    override fun <V> set(key: WasmHistoryBasedIncrementalCompilationConfiguration.Option<V>, value: V) {
+        options[key] = value
+    }
+
+    operator fun <V> get(key: Option<V>): V {
+        return options[key]
+    }
+
+    operator fun <V> set(key: Option<V>, value: V) {
+        options[key] = value
+    }
+
+    class Option<V> : BaseOptionWithDefault<V> {
+        constructor(id: String) : super(id)
+        constructor(id: String, default: V) : super(id, default = default)
+    }
+
+    companion object {
+        val ROOT_PROJECT_BUILD_DIR: Option<Path?> = Option("ROOT_PROJECT_BUILD_DIR", null)
+        val HISTORY_FILE_DIR: Option<Path?> = Option("HISTORY_FILE_DIR", null)
+    }
+}

ewok-pulse/saber-e2e-kotlin

compiler/build-tools/kotlin-build-tools-api/src/main/kotlin/org/jetbrains/kotlin/buildtools/api/wasm/operations/WasmKlibCompilationOperation.kt
@@ -1,0 +1,148 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.buildtools.api.wasm.operations
+
+import org.jetbrains.kotlin.buildtools.api.BaseCompilationOperation
+import org.jetbrains.kotlin.buildtools.api.CancellableBuildOperation
+import org.jetbrains.kotlin.buildtools.api.CompilationResult
+import org.jetbrains.kotlin.buildtools.api.ExperimentalBuildToolsApi
+import org.jetbrains.kotlin.buildtools.api.SourcesChanges
+import org.jetbrains.kotlin.buildtools.api.arguments.ExperimentalCompilerArgument
+import org.jetbrains.kotlin.buildtools.api.arguments.WasmArguments
+import org.jetbrains.kotlin.buildtools.api.internal.BaseOption
+import org.jetbrains.kotlin.buildtools.api.wasm.IncrementalModule
+import org.jetbrains.kotlin.buildtools.api.wasm.WasmHistoryBasedIncrementalCompilationConfiguration
+import org.jetbrains.kotlin.buildtools.api.wasm.WasmIncrementalCompilationConfiguration
+import java.nio.file.Path
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+/**
+ * Compiles Kotlin code targeting Wasm platform and using specified options into a klib.
+ *
+ * This interface is not intended to be implemented by the API consumers.
+ *
+ * Obtain an instance of this interface from [WasmPlatformToolchain.wasmKlibCompilationOperationBuilder].
+ *
+ * @since 2.4.20
+ */
+@ExperimentalBuildToolsApi
+public interface WasmKlibCompilationOperation : BaseCompilationOperation, CancellableBuildOperation<CompilationResult> {
+
+    /**
+     * All sources of the compilation unit.
+     */
+    public val sources: List<Path>
+
+    /**
+     * The directory where the resulting klib will be placed.
+     */
+    public val destination: Path
+
+    @OptIn(ExperimentalCompilerArgument::class)
+    public val compilerArguments: WasmArguments
+
+    /**
+     * A builder for configuring and instantiating the [WasmKlibCompilationOperation].
+     */
+    public interface Builder : BaseCompilationOperation.Builder {
+        /**
+         * All sources of the compilation unit.
+         */
+        public val sources: List<Path>
+
+        /**
+         * Output klib file.
+         */
+        public val destination: Path
+
+        /**
+         * Kotlin compiler configurable options for klib-based compilation.
+         */
+        @OptIn(ExperimentalCompilerArgument::class)
+        public override val compilerArguments: WasmArguments.Builder
+
+        /**
+         * Creates the configuration object for history-based incremental compilation (IC) in Wasm projects.
+         *
+         * @param rootProjectDir the root directory of the project
+         * @param workingDirectory the directory where the compiler will store the incremental compilation caches
+         * @param sourcesChanges the changes in the source files, which can be unknown, to-be-calculated, or known
+         * @param modulesInformation the information about modules layout in the project
+         */
+        public fun historyBasedIcConfigurationBuilder(
+            rootProjectDir: Path,
+            workingDirectory: Path,
+            sourcesChanges: SourcesChanges,
+            modulesInformation: List<IncrementalModule>,
+        ): WasmHistoryBasedIncrementalCompilationConfiguration.Builder
+
+        /**
+         * Get the value for option specified by [key] if it was previously [set] or if it has a default value.
+         *
+         * @return the previously set value for an option
+         * @throws IllegalStateException if the option was not set and has no default value
+         */
+        public operator fun <V> get(key: Option<V>): V
+
+        /**
+         * Set the [value] for option specified by [key], overriding any previous value for that option.
+         */
+        public operator fun <V> set(key: Option<V>, value: V)
+
+        /**
+         * Creates an immutable instance of [WasmKlibCompilationOperation] based on the configuration of this builder.
+         */
+        public override fun build(): WasmKlibCompilationOperation
+    }
+
+    /**
+     * Returns a [Builder] initialized with the values of this [WasmKlibCompilationOperation].
+     */
+    public fun toBuilder(): Builder
+
+    /**
+     * An option for configuring a [WasmKlibCompilationOperation].
+     */
+    public class Option<V> internal constructor(id: String) : BaseOption<V>(id)
+
+    /**
+     * Get the value for option specified by [key] if it was previously [set] or if it has a default value.
+     *
+     * @return the previously set value for an option
+     * @throws IllegalStateException if the option was not set and has no default value
+     */
+    public operator fun <V> get(key: Option<V>): V
+
+    public companion object {
+        @JvmField
+        public val INCREMENTAL_COMPILATION: Option<WasmIncrementalCompilationConfiguration?> = Option("INCREMENTAL_COMPILATION")
+    }
+}
+
+
+/**
+ * Convenience function for creating a [WasmKlibCompilationOperation] with options configured by [builderAction].
+ *
+ * @return an immutable `WasmKlibCompilationOperation`.
+ * @see WasmPlatformToolchain.wasmKlibCompilationOperationBuilder
+ */
+@OptIn(ExperimentalContracts::class)
+@ExperimentalBuildToolsApi
+public inline fun WasmKlibCompilationOperation.Builder.historyBasedIcConfiguration(
+    rootProjectDir: Path,
+    workingDirectory: Path,
+    sourcesChanges: SourcesChanges,
+    modulesInformation: List<IncrementalModule>,
+    builderAction: WasmHistoryBasedIncrementalCompilationConfiguration.Builder.() -> Unit = {},
+): WasmHistoryBasedIncrementalCompilationConfiguration {
+    contract {
+        callsInPlace(builderAction, InvocationKind.EXACTLY_ONCE)
+    }
+    return historyBasedIcConfigurationBuilder(rootProjectDir, workingDirectory, sourcesChanges, modulesInformation).apply(builderAction)
+        .build()
+}

compiler/build-tools/kotlin-build-tools-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/wasm/WasmHistoryBasedIncrementalCompilationConfigurationImpl.kt
@@ -1,0 +1,90 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("DEPRECATION")
+
+package org.jetbrains.kotlin.buildtools.internal.wasm
+
+import org.jetbrains.kotlin.buildtools.api.SourcesChanges
+import org.jetbrains.kotlin.buildtools.api.wasm.IncrementalModule
+import org.jetbrains.kotlin.buildtools.api.wasm.WasmHistoryBasedIncrementalCompilationConfiguration
+import org.jetbrains.kotlin.buildtools.internal.*
+import org.jetbrains.kotlin.incremental.IncrementalCompilerRunner
+import java.nio.file.Path
+
+internal class WasmHistoryBasedIncrementalCompilationConfigurationImpl private constructor(
+    override val workingDirectory: Path,
+    override val sourcesChanges: SourcesChanges,
+    override val modulesInformation: List<IncrementalModule>,
+    override val options: Options = Options(
+        WasmHistoryBasedIncrementalCompilationConfiguration::class
+    ),
+) : BaseIncrementalCompilationConfigurationImpl(), WasmHistoryBasedIncrementalCompilationConfiguration,
+    WasmHistoryBasedIncrementalCompilationConfiguration.Builder,
+    DeepCopyable<WasmHistoryBasedIncrementalCompilationConfigurationImpl> {
+
+    constructor(
+        rootProjectDir: Path,
+        workingDirectory: Path,
+        sourcesChanges: SourcesChanges,
+        modulesInformation: List<IncrementalModule>,
+    ) : this(
+        workingDirectory,
+        sourcesChanges,
+        modulesInformation,
+        Options(WasmHistoryBasedIncrementalCompilationConfiguration::class)
+    ) {
+        initializeOptions(this::class, options)
+        options[ROOT_PROJECT_DIR] = rootProjectDir
+    }
+
+    internal val historyFile: Path
+        get() {
+            return (this[HISTORY_FILE_DIR] ?: workingDirectory).resolve(
+                IncrementalCompilerRunner.BUILD_HISTORY_FILE_NAME
+            )
+        }
+
+    override fun build(): WasmHistoryBasedIncrementalCompilationConfiguration = deepCopy()
+
+    override fun toBuilder(): WasmHistoryBasedIncrementalCompilationConfiguration.Builder = deepCopy()
+
+    override fun deepCopy(): WasmHistoryBasedIncrementalCompilationConfigurationImpl =
+        WasmHistoryBasedIncrementalCompilationConfigurationImpl(
+            workingDirectory,
+            sourcesChanges,
+            modulesInformation,
+            options.deepCopy()
+        )
+
+
+    @UseFromImplModuleRestricted
+    override fun <V> get(key: WasmHistoryBasedIncrementalCompilationConfiguration.Option<V>): V {
+        return options[key]
+    }
+
+    @UseFromImplModuleRestricted
+    override fun <V> set(key: WasmHistoryBasedIncrementalCompilationConfiguration.Option<V>, value: V) {
+        options[key] = value
+    }
+
+    operator fun <V> get(key: Option<V>): V {
+        return options[key]
+    }
+
+    operator fun <V> set(key: Option<V>, value: V) {
+        options[key] = value
+    }
+
+    class Option<V> : BaseOptionWithDefault<V> {
+        constructor(id: String) : super(id)
+        constructor(id: String, default: V) : super(id, default = default)
+    }
+
+    companion object {
+        val ROOT_PROJECT_BUILD_DIR: Option<Path?> = Option("ROOT_PROJECT_BUILD_DIR", null)
+        val HISTORY_FILE_DIR: Option<Path?> = Option("HISTORY_FILE_DIR", null)
+    }
+}
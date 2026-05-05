JetBrains/kotlin

commit 80bbc4b61da455daa8a65b30afb127da5870b2a9

Reflection: support descriptor-less Kotlin constructors
 #KT-80710

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KotlinKFunction.kt
@@ -5,20 +5,21 @@
 
 package kotlin.reflect.jvm.internal
 
-import java.lang.reflect.AnnotatedElement
-import java.lang.reflect.Method
-import java.lang.reflect.Modifier
+import java.lang.reflect.*
 import kotlin.LazyThreadSafetyMode.PUBLICATION
 import kotlin.jvm.internal.FunctionBase
 import kotlin.metadata.KmType
 import kotlin.metadata.KmValueParameter
 import kotlin.metadata.jvm.JvmMethodSignature
+import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.KTypeParameter
-import kotlin.reflect.jvm.internal.calls.Caller
-import kotlin.reflect.jvm.internal.calls.CallerImpl
-import kotlin.reflect.jvm.internal.calls.arity
-import kotlin.reflect.jvm.internal.calls.createValueClassAwareCallerIfNeeded
+import kotlin.reflect.KVisibility
+import kotlin.reflect.jvm.internal.calls.*
+import kotlin.reflect.jvm.internal.calls.AnnotationConstructorCaller.CallMode.CALL_BY_NAME
+import kotlin.reflect.jvm.internal.calls.AnnotationConstructorCaller.CallMode.POSITIONAL_CALL
+import kotlin.reflect.jvm.internal.calls.AnnotationConstructorCaller.Origin.KOTLIN
+import kotlin.reflect.jvm.jvmErasure
 
 internal abstract class KotlinKFunction(
     override val container: KDeclarationContainerImpl,
@@ -60,26 +61,51 @@ internal abstract class KotlinKFunction(
         }
 
     override val caller: Caller<*> by lazy(PUBLICATION) {
-        require(container is KPackageImpl) { "Only top-level functions are supported for now: $this" }
+        require(isConstructor || container is KPackageImpl) { "Only constructors and top-level functions are supported for now: $this" }
         val signature = jvmSignature
-        val member = container.findMethodBySignature(signature.name, signature.descriptor) as Method
-        createStaticMethodCaller(member, isCallByToValueClassMangledMethod = false)
-            .createValueClassAwareCallerIfNeeded(this, isDefault = false, forbidUnboxingForIndices = emptyList())
+        val member: Member? =
+            if (isConstructor && !container.isInlineClass()) {
+                if (isAnnotationConstructor)
+                    return@lazy AnnotationConstructorCaller(container.jClass, parameters.map { it.name!! }, POSITIONAL_CALL, KOTLIN)
+                container.findConstructorBySignature(signature.descriptor)
+            } else container.findMethodBySignature(signature.name, signature.descriptor)
+
+        when (member) {
+            is Constructor<*> -> createConstructorCaller(member, isDefault = false)
+            is Method -> createStaticMethodCaller(member, isCallByToValueClassMangledMethod = false)
+            else -> throw KotlinReflectionInternalError("Could not compute caller for function: $this")
+        }.createValueClassAwareCallerIfNeeded(this, isDefault = false, forbidUnboxingForIndices = emptyList())
     }
 
     override val defaultCaller: Caller<*>? by lazy(PUBLICATION) {
-        require(container is KPackageImpl) { "Only top-level functions are supported for now: $this" }
+        require(isConstructor || container is KPackageImpl) { "Only constructors and top-level functions are supported for now: $this" }
         val signature = jvmSignature
-        val patchingResult = patchJvmDescriptorByExtraBoxing(this, signature.descriptor)
-        val member = container.findDefaultMethod(
-            signature.name, patchingResult.newDescriptor, !Modifier.isStatic(caller.member!!.modifiers),
-            allParameters.any { it.kind == KParameter.Kind.EXTENSION_RECEIVER },
-        )
+        val preventUnboxingForIndices = mutableListOf<Int>()
+        val member: Member? =
+            if (isConstructor && !container.isInlineClass()) {
+                if (isAnnotationConstructor)
+                    return@lazy AnnotationConstructorCaller(container.jClass, parameters.map { it.name!! }, CALL_BY_NAME, KOTLIN)
+                val patchingResult = patchJvmDescriptorByExtraBoxing(this, jvmSignature.descriptor)
+                preventUnboxingForIndices.addAll(patchingResult.boxedIndices)
+                container.findDefaultConstructor(patchingResult.newDescriptor) as Member?
+            } else {
+                val patchingResult = patchJvmDescriptorByExtraBoxing(this, signature.descriptor)
+                preventUnboxingForIndices.addAll(patchingResult.boxedIndices)
+                container.findDefaultMethod(
+                    signature.name, patchingResult.newDescriptor, !Modifier.isStatic(caller.member!!.modifiers),
+                    allParameters.any { it.kind == KParameter.Kind.EXTENSION_RECEIVER },
+                )
+            }
 
-        member?.let {
-            createStaticMethodCaller(it, isCallByToValueClassMangledMethod = caller.isBoundInstanceCallWithValueClasses)
-        }?.createValueClassAwareCallerIfNeeded(this, isDefault = true, forbidUnboxingForIndices = patchingResult.boxedIndices.toList())
+        when (member) {
+            is Constructor<*> -> createConstructorCaller(member, isDefault = true)
+            is Method -> createStaticMethodCaller(member, isCallByToValueClassMangledMethod = caller.isBoundInstanceCallWithValueClasses)
+            else -> null
+        }?.createValueClassAwareCallerIfNeeded(this, isDefault = true, forbidUnboxingForIndices = preventUnboxingForIndices)
     }
+
+    private fun KDeclarationContainerImpl.isInlineClass(): Boolean =
+        this is KClassImpl<*> && isValue
 
     // boundReceiver is unboxed receiver when the receiver is inline class.
     // However, when the expected dispatch receiver type is an interface,
@@ -95,6 +121,27 @@ internal abstract class KotlinKFunction(
                 member, isCallByToValueClassMangledMethod, if (useBoxedBoundReceiver(member)) rawBoundReceiver else boundReceiver
             )
         else CallerImpl.Method.Static(member)
+
+    private fun createConstructorCaller(member: Constructor<*>, isDefault: Boolean): CallerImpl<Constructor<*>> {
+        return if (!isDefault && this is KotlinKConstructor && shouldHideConstructorDueToValueClassTypeValueParameters(this)) {
+            if (isBound)
+                CallerImpl.AccessorForHiddenBoundConstructor(member, boundReceiver)
+            else
+                CallerImpl.AccessorForHiddenConstructor(member)
+        } else {
+            if (isBound)
+                CallerImpl.BoundConstructor(member, boundReceiver)
+            else
+                CallerImpl.Constructor(member)
+        }
+    }
+
+    private fun shouldHideConstructorDueToValueClassTypeValueParameters(constructor: KotlinKConstructor): Boolean =
+        constructor.visibility != KVisibility.PRIVATE &&
+                constructor.parameters.any { it.type.jvmErasure.isValueClassThatRequiresMangling() }
+
+    private fun KClass<*>.isValueClassThatRequiresMangling(): Boolean =
+        isValue && this != Result::class
 
     override fun equals(other: Any?): Boolean {
         val that = other.asReflectFunction() ?: return false

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLBlackBoxTestGenerated.java
@@ -54511,6 +54511,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
       }
 
       @Test
+      @TestMetadata("constructorParameters.kt")
+      public void testConstructorParameters() {
+        runTest("compiler/testData/codegen/box/reflection/annotations/constructorParameters.kt");
+      }
+
+      @Test
       @TestMetadata("findAnnotation.kt")
       public void testFindAnnotation() {
         runTest("compiler/testData/codegen/box/reflection/annotations/findAnnotation.kt");
@@ -55114,6 +55120,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
         }
 
         @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/result.kt");
+        }
+
+        @Test
         @TestMetadata("secondaryConstructorVisibilies.kt")
         public void testSecondaryConstructorVisibilies() {
           runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/secondaryConstructorVisibilies.kt");
@@ -55661,6 +55673,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
           KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/codegen/box/reflection/callBy/inlineClasses"), Pattern.compile("^(.+)\\.kt$"), null, true);
         }
 
+        @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/callBy/inlineClasses/result.kt");
+        }
+
         @Nested
         @TestMetadata("compiler/testData/codegen/box/reflection/callBy/inlineClasses/nonNullObject")
         @TestDataPath("$PROJECT_ROOT")
@@ -56179,6 +56197,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
       }
 
       @Test
+      @TestMetadata("innerClassOfGenericClass.kt")
+      public void testInnerClassOfGenericClass() {
+        runTest("compiler/testData/codegen/box/reflection/constructors/innerClassOfGenericClass.kt");
+      }
+
+      @Test
       @TestMetadata("primaryConstructor.kt")
       public void testPrimaryConstructor() {
         runTest("compiler/testData/codegen/box/reflection/constructors/primaryConstructor.kt");
@@ -56592,12 +56616,6 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
       @TestMetadata("constructor.kt")
       public void testConstructor() {
         runTest("compiler/testData/codegen/box/reflection/mapping/constructor.kt");
-      }
-
-      @Test
-      @TestMetadata("constructorWithInlineClassParameters.kt")
-      public void testConstructorWithInlineClassParameters() {
-        runTest("compiler/testData/codegen/box/reflection/mapping/constructorWithInlineClassParameters.kt");
       }
 
       @Test

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedBlackBoxTestGenerated.java
@@ -54511,6 +54511,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
       }
 
       @Test
+      @TestMetadata("constructorParameters.kt")
+      public void testConstructorParameters() {
+        runTest("compiler/testData/codegen/box/reflection/annotations/constructorParameters.kt");
+      }
+
+      @Test
       @TestMetadata("findAnnotation.kt")
       public void testFindAnnotation() {
         runTest("compiler/testData/codegen/box/reflection/annotations/findAnnotation.kt");
@@ -55114,6 +55120,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
         }
 
         @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/result.kt");
+        }
+
+        @Test
         @TestMetadata("secondaryConstructorVisibilies.kt")
         public void testSecondaryConstructorVisibilies() {
           runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/secondaryConstructorVisibilies.kt");
@@ -55661,6 +55673,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
           KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/codegen/box/reflection/callBy/inlineClasses"), Pattern.compile("^(.+)\\.kt$"), null, true);
         }
 
+        @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/callBy/inlineClasses/result.kt");
+        }
+
         @Nested
         @TestMetadata("compiler/testData/codegen/box/reflection/callBy/inlineClasses/nonNullObject")
         @TestDataPath("$PROJECT_ROOT")
@@ -56179,6 +56197,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
       }
 
       @Test
+      @TestMetadata("innerClassOfGenericClass.kt")
+      public void testInnerClassOfGenericClass() {
+        runTest("compiler/testData/codegen/box/reflection/constructors/innerClassOfGenericClass.kt");
+      }
+
+      @Test
       @TestMetadata("primaryConstructor.kt")
       public void testPrimaryConstructor() {
         runTest("compiler/testData/codegen/box/reflection/constructors/primaryConstructor.kt");
@@ -56592,12 +56616,6 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
       @TestMetadata("constructor.kt")
       public void testConstructor() {
         runTest("compiler/testData/codegen/box/reflection/mapping/constructor.kt");
-      }
-
-      @Test
-      @TestMetadata("constructorWithInlineClassParameters.kt")
-      public void testConstructorWithInlineClassParameters() {
-        runTest("compiler/testData/codegen/box/reflection/mapping/constructorWithInlineClassParameters.kt");
       }
 
       @Test

compiler/testData/codegen/box/reflection/annotations/constructorParameters.kt
@@ -1,0 +1,60 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+// FILE: test/J.java
+package test;
+
+public class J {
+    public J(@Anno("J") String s) {}
+
+    public class Inner {
+        public Inner(@Anno("Inner") int x) {}
+    }
+}
+
+// FILE: test/JEnum.java
+package test;
+
+public enum JEnum {
+    ;
+    JEnum(@Anno("JEnum") double d) {}
+}
+
+// FILE: test/box.kt
+package test
+
+import kotlin.reflect.KClass
+import kotlin.test.assertEquals
+
+annotation class Anno(val value: String)
+
+class K(@Anno("K") f: Float) {
+    inner class Inner(@Anno("Inner") j: Long)
+}
+
+enum class KEnum(@Anno("KEnum") z: Boolean)
+
+sealed class Sealed(@Anno("Sealed") s: Short)
+
+class Default(@Anno("Default") n: Number? = null)
+
+sealed class SealedWithDefault(@Anno("SealedWithDefault") a: Any? = null)
+
+private val KClass<*>.ctorParamAnnotations: String
+    get() = constructors.single().parameters.joinToString(", ") { p ->
+        p.annotations.map { (it as Anno).value }.toString()
+    }
+
+fun box(): String {
+    assertEquals("[J]", J::class.ctorParamAnnotations)
+    assertEquals("[], [Inner]", J.Inner::class.ctorParamAnnotations)
+    assertEquals("[JEnum]", JEnum::class.ctorParamAnnotations)
+
+    assertEquals("[K]", K::class.ctorParamAnnotations)
+    assertEquals("[], [Inner]", K.Inner::class.ctorParamAnnotations)
+    assertEquals("[KEnum]", KEnum::class.ctorParamAnnotations)
+    assertEquals("[Sealed]", Sealed::class.ctorParamAnnotations)
+    assertEquals("[Default]", Default::class.ctorParamAnnotations)
+    assertEquals("[SealedWithDefault]", SealedWithDefault::class.ctorParamAnnotations)
+
+    return "OK"
+}

compiler/testData/codegen/box/reflection/call/inlineClasses/result.kt
@@ -1,0 +1,18 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+
+import kotlin.reflect.jvm.isAccessible
+
+class C(val x: Result<Int>)
+
+fun box(): String {
+    val resultCtor = Result::class.constructors.single()
+    val r = resultCtor.apply { isAccessible = true }.call(42)
+    if (r != Result.success(42)) return "Fail 1: $r"
+
+    val ctorWithResult = ::C
+    val s = ctorWithResult.call(r).x
+    if (s != Result.success(42)) return "Fail 2: $s"
+
+    return "OK"
+}

compiler/testData/codegen/box/reflection/callBy/inlineClasses/result.kt
@@ -1,0 +1,18 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+
+import kotlin.reflect.jvm.isAccessible
+
+class C(val x: Result<Int>)
+
+fun box(): String {
+    val resultCtor = Result::class.constructors.single()
+    val r = resultCtor.apply { isAccessible = true }.callBy(mapOf(resultCtor.parameters.single() to 42))
+    if (r != Result.success(42)) return "Fail 1: $r"
+
+    val ctorWithResult = ::C
+    val s = ctorWithResult.callBy(mapOf(ctorWithResult.parameters.single() to r)).x
+    if (s != Result.success(42)) return "Fail 2: $s"
+
+    return "OK"
+}

compiler/testData/codegen/box/reflection/constructors/classesWithoutConstructors.kt
@@ -2,7 +2,7 @@
 // WITH_REFLECT
 
 import kotlin.coroutines.SuspendFunction0
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 interface Interface
 object Obj
@@ -12,12 +12,12 @@ class C {
 }
 
 fun box(): String {
-    assertTrue(Interface::class.constructors.isEmpty())
-    assertTrue(Obj::class.constructors.isEmpty())
-    assertTrue(C.Companion::class.constructors.isEmpty())
-    assertTrue(object {}::class.constructors.isEmpty())
-    assertTrue(Function0::class.constructors.isEmpty())
-    assertTrue(SuspendFunction0::class.constructors.isEmpty())
+    assertEquals(emptyList(), Interface::class.constructors)
+    assertEquals(emptyList(), Obj::class.constructors)
+    assertEquals(emptyList(), C.Companion::class.constructors)
+    assertEquals(emptyList(), object {}::class.constructors)
+    assertEquals(emptyList(), Function0::class.constructors)
+    assertEquals(emptyList(), SuspendFunction0::class.constructors)
 
     return "OK"
 }

compiler/testData/codegen/box/reflection/constructors/innerClassOfGenericClass.kt
@@ -1,0 +1,20 @@
+// TARGET_BACKEND: JVM
+// IGNORE_BACKEND_K1: JVM_IR
+// WITH_REFLECT
+package test
+
+import kotlin.test.assertEquals
+
+class C<A, B> {
+    class Nested
+    inner class Inner(a: A)
+}
+
+fun box(): String {
+    assertEquals("fun `<init>`(): test.C.Nested", C<*, *>::Nested.toString())
+    assertEquals("fun test.C<A, B>.`<init>`(A): test.C<A, B>.Inner", C<*, *>::Inner.toString())
+
+    assertEquals(C::class.typeParameters, C<*, *>::Inner.returnType.arguments.map { it.type?.classifier })
+
+    return "OK"
+}

ewok-pulse/saber-e2e-kotlin

commit 144dba2b342b7163f1a41ea62538acdb3d577b2e

Reflection: fix `call` for sealed class constructors
Sealed class cannot be created reflectively, since it's abstract, but
the call should fail with the proper exception (InstantiationException)
instead of kotlin-reflect-specific internal error.

The added check is the same check that was present in K1, see
`shouldHideConstructorDueToValueClassTypeValueParameters` in
`inlineClassManglingRules.kt`.

 #RCA: caused by 80bbc4b61d (part of KT-80710).

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KotlinKFunction.kt
@@ -143,6 +143,7 @@ internal abstract class KotlinKFunction(
 
     private fun shouldHideConstructorDueToValueClassTypeValueParameters(constructor: KotlinKConstructor): Boolean =
         constructor.visibility != KVisibility.PRIVATE &&
+                !(constructor.container as KClass<*>).isSealed &&
                 constructor.parameters.any { it.type.jvmErasure.isValueClassThatRequiresMangling() }
 
     private fun KClass<*>.isValueClassThatRequiresMangling(): Boolean =

compiler/testData/codegen/boxJvm/reflection/call/sealedClassConstructor.kt
@@ -1,0 +1,21 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+// FULL_JDK
+
+import kotlin.reflect.jvm.isAccessible
+
+@JvmInline
+value class Z(val value: Int)
+
+sealed class S(val z: Z)
+
+fun box(): String {
+    val ctor = S::class.constructors.first()
+    ctor.isAccessible = true
+    try {
+        ctor.call(Z(1))
+    } catch (e: InstantiationException) {
+        return "OK"
+    }
+    return "Fail"
+}
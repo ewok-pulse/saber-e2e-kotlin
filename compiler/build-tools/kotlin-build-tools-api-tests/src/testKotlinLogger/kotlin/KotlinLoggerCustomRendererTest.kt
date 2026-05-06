Stream29/kotlin

[BTA] KT-85567 Expose diagnostic IDs in renderer transport
Build Tools API renderers need the structured compiler diagnostic
identifier so downstream consumers can distinguish real diagnostics
without parsing rendered text. This adds an opt-in renderer extension,
threads the identifier through the BTA adapters, regenerates the API
dump, and covers both the structured and null fallback paths with tests.

^KT-85567 Fixed

compiler/build-tools/kotlin-build-tools-api-tests/src/testKotlinLogger/kotlin/KotlinLoggerCustomRendererTest.kt
@@ -7,12 +7,14 @@ package org.jetbrains.kotlin.buildtools.tests.compilation
 
 import org.jetbrains.kotlin.buildtools.api.BaseCompilationOperation
 import org.jetbrains.kotlin.buildtools.api.CompilerMessageRenderer
+import org.jetbrains.kotlin.buildtools.api.CompilerMessageRendererWithDiagnosticId
 import org.jetbrains.kotlin.buildtools.api.CompilerMessageRenderer.Severity
 import org.jetbrains.kotlin.buildtools.api.CompilerMessageRenderer.SourceLocation
 import org.jetbrains.kotlin.buildtools.tests.CompilerExecutionStrategyConfiguration
 import org.jetbrains.kotlin.buildtools.tests.compilation.model.BtaV2StrategyAgnosticCompilationTest
 import org.jetbrains.kotlin.buildtools.tests.compilation.model.LogLevel
 import org.jetbrains.kotlin.buildtools.tests.compilation.model.project
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 
@@ -37,6 +39,32 @@ class KotlinLoggerCustomRendererTest : BaseCompilationTest() {
                 assertTrue(warnLines.any { "[CUSTOM WARNING]" in it && "deprecated" in it.lowercase() }) {
                     "Expected custom-rendered deprecation warning at WARN level, got: $warnLines"
                 }
+            }
+        }
+    }
+
+    @BtaV2StrategyAgnosticCompilationTest
+    @DisplayName("Custom renderer receives compiler diagnostic identifier")
+    fun customRendererReceivesDiagnosticIdentifier(strategyConfig: CompilerExecutionStrategyConfiguration) {
+        val diagnosticIds = mutableListOf<String?>()
+        val renderer = object : CompilerMessageRendererWithDiagnosticId {
+            override fun render(severity: Severity, message: String, location: SourceLocation?, diagnosticId: String?): String {
+                diagnosticIds += diagnosticId
+                return "[CUSTOM $severity][$diagnosticId] $message"
+            }
+        }
+
+        project(strategyConfig) {
+            val module = module("compilation-error")
+            module.compile(compilationConfigAction = {
+                it[BaseCompilationOperation.COMPILER_MESSAGE_RENDERER] = renderer
+            }) {
+                expectFail()
+                val errorLines = logLines[LogLevel.ERROR].orEmpty()
+                assertTrue(errorLines.any { "[CUSTOM ERROR][UNRESOLVED_REFERENCE]" in it }) {
+                    "Expected custom-rendered unresolved reference error at ERROR level, got: $errorLines"
+                }
+                assertEquals(listOf("UNRESOLVED_REFERENCE"), diagnosticIds.filterNotNull().distinct())
             }
         }
     }

ewok-pulse/saber-e2e-kotlin

compiler/build-tools/kotlin-build-tools-api-tests/src/testKotlinLogger/kotlin/KotlinLoggerCustomRendererTest.kt
@@ -7,12 +7,14 @@ package org.jetbrains.kotlin.buildtools.tests.compilation
 
 import org.jetbrains.kotlin.buildtools.api.BaseCompilationOperation
 import org.jetbrains.kotlin.buildtools.api.CompilerMessageRenderer
+import org.jetbrains.kotlin.buildtools.api.CompilerMessageRendererWithDiagnosticId
 import org.jetbrains.kotlin.buildtools.api.CompilerMessageRenderer.Severity
 import org.jetbrains.kotlin.buildtools.api.CompilerMessageRenderer.SourceLocation
 import org.jetbrains.kotlin.buildtools.tests.CompilerExecutionStrategyConfiguration
 import org.jetbrains.kotlin.buildtools.tests.compilation.model.BtaV2StrategyAgnosticCompilationTest
 import org.jetbrains.kotlin.buildtools.tests.compilation.model.LogLevel
 import org.jetbrains.kotlin.buildtools.tests.compilation.model.jvmProject
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 
@@ -37,6 +39,32 @@ class KotlinLoggerCustomRendererTest : BaseCompilationTest() {
                 assertTrue(warnLines.any { "[CUSTOM WARNING]" in it && "deprecated" in it.lowercase() }) {
                     "Expected custom-rendered deprecation warning at WARN level, got: $warnLines"
                 }
+            }
+        }
+    }
+
+    @BtaV2StrategyAgnosticCompilationTest
+    @DisplayName("Custom renderer receives compiler diagnostic identifier")
+    fun customRendererReceivesDiagnosticIdentifier(strategyConfig: CompilerExecutionStrategyConfiguration) {
+        val diagnosticIds = mutableListOf<String?>()
+        val renderer = object : CompilerMessageRendererWithDiagnosticId {
+            override fun render(severity: Severity, message: String, location: SourceLocation?, diagnosticId: String?): String {
+                diagnosticIds += diagnosticId
+                return "[CUSTOM $severity][$diagnosticId] $message"
+            }
+        }
+
+        jvmProject(strategyConfig) {
+            val module = module("compilation-error")
+            module.compile(compilationConfigAction = {
+                it[BaseCompilationOperation.COMPILER_MESSAGE_RENDERER] = renderer
+            }) {
+                expectFail()
+                val errorLines = logLines[LogLevel.ERROR].orEmpty()
+                assertTrue(errorLines.any { "[CUSTOM ERROR][UNRESOLVED_REFERENCE]" in it }) {
+                    "Expected custom-rendered unresolved reference error at ERROR level, got: $errorLines"
+                }
+                assertEquals(listOf("UNRESOLVED_REFERENCE"), diagnosticIds.filterNotNull().distinct())
             }
         }
     }